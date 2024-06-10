2349afd4293746f6387afdd989f4ec7b01eda35d
hacker-git-code <113529222+hacker-git-code@users.noreply.github.com>
2024-06-10 09:42:43 +0530

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'REHAN MOMIN';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'WELCOME TO THE OFFICIAL WEBSITE';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'HACKER @All rights reserved.';
 
   return {
     name,
